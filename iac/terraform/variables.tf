8b15b575b816c98ef31d413450152dc0c0d3c615
kurtening <kurtranzelflores@gmail.com>
2023-11-20 14:05:26 +0800

    Add required variables for the root module

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -2,3 +2,29 @@ variable "github_token" {
   description = "The GitHub token"
   type        = string
 }
+
+variable "region" {
+  description = "The Azure region"
+  type        = string
+}
+
+variable "region_code" {
+  description = "The region code for naming conventions"
+  type        = string
+}
+
+variable "project_code" {
+  description = "The project code for naming conventions"
+  type        = string
+}
+
+variable "env_code" {
+  description = "The environment code for naming conventions"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (e.g., development, production)"
+  type        = string
+}
+
