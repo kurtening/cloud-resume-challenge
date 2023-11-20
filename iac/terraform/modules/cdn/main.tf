777e223dc3e9b105ed336f78266c6c4992c091ab
kurtening <kurtranzelflores@gmail.com>
2023-11-20 14:23:38 +0800

    Add resource declarations and required variables for the cdn module

---
+++ iac/terraform/modules/cdn/main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_cdn_profile" "cdn_profile" {
+  name                = var.cdn_profile_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+}
+
+resource "azurerm_cdn_endpoint" "cdn_endpoint" {
+  name                = var.cdn_endpoint_name
+  profile_name        = azurerm_cdn_profile.cdn_profile.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  origin {
+    name      = var.cdn_endpoint_origin_name
+    host_name = var.cdn_endpoint_origin_host_name
+  }
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "cdn_endpoint_custom_domain" {
+  name            = var.cdn_endpoint_custom_domain_name
+  cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
+  host_name       = var.cdn_endpoint_custom_domain_host_name
+  cdn_managed_https {
+    certificate_type = "Dedicated"
+    protocol_type    = "ServerNameIndication"
+    tls_version      = "TLS12"
+  }
+}
