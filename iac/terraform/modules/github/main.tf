59dbb67464dfe0e723a65c1882c9084b96339f55
kurtening <kurtranzelflores@gmail.com>
2023-11-24 07:51:06 +0800

    Fix environment secret provider resource name

--- iac/terraform/modules/github/main.tf
+++ iac/terraform/modules/github/main.tf
@@ -8,7 +8,7 @@ resource "github_actions_secret" "function_app_publishing_profile" {
   plaintext_value = var.function_app_publishing_profile[0].password
 }
 
-resource "github_actions_environment" "storage_account_access_key" {
+resource "github_actions_secret" "storage_account_access_key" {
   repository      = data.github_repository.repository.name
   secret_name     = "SA_ACCESS_KEY"
   plaintext_value = var.storage_account_access_key
