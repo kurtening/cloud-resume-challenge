fe0decea670813b660b1416835f548a0782729ee
kurtening <kurtranzelflores@gmail.com>
2023-11-24 07:45:28 +0800

    Convert environment variables to respository variables

--- iac/terraform/modules/github/main.tf
+++ iac/terraform/modules/github/main.tf
@@ -8,13 +8,13 @@ resource "github_actions_secret" "function_app_publishing_profile" {
   plaintext_value = var.function_app_publishing_profile[0].password
 }
 
-resource "github_actions_environment_secret" "storage_account_access_key" {
+resource "github_actions_environment" "storage_account_access_key" {
   repository      = data.github_repository.repository.name
   secret_name     = "SA_ACCESS_KEY"
   plaintext_value = var.storage_account_access_key
 }
 
-resource "github_actions_environment_variable" "function_app_name" {
+resource "github_actions_variable" "function_app_name" {
   repository = data.github_repository.repository.name
   variable_name = "FA_NAME"
   value = replace(var.function_app_publishing_profile[0].name, "$", "")
