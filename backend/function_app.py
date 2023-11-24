73d060826472cdf5cdfb67b9631f5829a5c118c7
kurtening <kurtranzelflores@gmail.com>
2023-11-24 08:19:24 +0800

    Initialize function app instance

--- backend/function_app.py
+++ backend/function_app.py
@@ -7,3 +7,7 @@ import os
 CONNECTION_STRING = os.environ.get("cosmosdb_account_connection_string")
 TABLE_NAME = "crc_visitor_table"
 
+# Initialize the function app
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+
