6d1025ef30f1302064e1619977471b63867d6190
kurtening <kurtranzelflores@gmail.com>
2023-11-24 08:19:08 +0800

    Declare constants

--- backend/function_app.py
+++ backend/function_app.py
@@ -3,3 +3,7 @@ import logging
 from azure.cosmosdb.table import TableService, models
 import os
 
+# Constants
+CONNECTION_STRING = os.environ.get("cosmosdb_account_connection_string")
+TABLE_NAME = "crc_visitor_table"
+
