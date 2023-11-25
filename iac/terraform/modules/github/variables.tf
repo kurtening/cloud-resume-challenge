e596c38bc7c734a69e30971170da79a1c6e3b7c0
kurtening <kurtranzelflores@gmail.com>
2023-11-25 13:52:53 +0800

    Remove azure sp credentials module variable

--- iac/terraform/modules/github/variables.tf
+++ iac/terraform/modules/github/variables.tf
@@ -12,20 +12,3 @@ variable "storage_account_access_key" {
   description = "Value for the SA_ACCESS_KEY secret."
   type        = string
 }
-
-variable "azure_credentials" {
-  description = "Value for the Azure SP Credentials"
-  type        = {
-    clientId: string
-    clientSecret: string
-    subscriptionId: string
-    tenantId: string
-  }
-  sensitive = true
-  default = {
-    clientId = var.ARM_CLIENT_ID
-    clientSecret = var.ARM_CLIENT_SECRET
-    subscriptionId = var.ARM_SUBSCRIPTION_ID
-    tenantId = var.ARM_TENANT_ID
-  }
-}
