0059826c02149804c5f6fcee37686fb716fc0358
kurtening <kurtranzelflores@gmail.com>
2023-11-25 14:01:02 +0800

    Add azure credentials to repository secret (#10)
    * Define azure credentials variable
    
    * Declare new github repository secret for azure credentials
    
    * Fix function app name
    
    * changed default value for azure credentials
    
    * Remove azure sp credentials module variable

--- iac/terraform/modules/github/main.tf
+++ iac/terraform/modules/github/main.tf
@@ -8,6 +8,17 @@ resource "github_actions_secret" "storage_account_access_key" {
   plaintext_value = var.storage_account_access_key
 }
 
+resource "github_actions_secret" "azure_credentials" {
+  repository  = data.github_repository.repository.name
+  secret_name = "AZURE_CREDENTIALS"
+  plaintext_value = jsondecode({
+    clientId       = var.ARM_CLIENT_ID
+    clientSecret   = var.ARM_CLIENT_SECRET
+    subscriptionId = var.ARM_SUBSCRIPTION_ID
+    tenantId       = var.ARM_TENANT_ID
+  })
+}
+
 resource "github_actions_variable" "function_app_name" {
   repository    = data.github_repository.repository.name
   variable_name = "FA_NAME"
