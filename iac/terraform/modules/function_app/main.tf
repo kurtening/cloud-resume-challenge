4a16aa3e5eb84699b601c76470a46ff40572db9a
kurtening <kurtranzelflores@gmail.com>
2023-11-30 05:19:04 +0800

    Add function runtime and SCM BUILD during deployment (#17)

--- iac/terraform/modules/function_app/main.tf
+++ iac/terraform/modules/function_app/main.tf
@@ -7,17 +7,18 @@ resource "azurerm_service_plan" "app_service_plan" {
 }
 
 resource "azurerm_linux_function_app" "function_app" {
-  name                       = var.function_app_name
-  resource_group_name        = var.resource_group_name
-  location                   = var.region
-  storage_account_name       = var.storage_account_name
-  storage_account_access_key = var.storage_account_access_key
-  service_plan_id            = azurerm_service_plan.app_service_plan.id
-
+  name                        = var.function_app_name
+  resource_group_name         = var.resource_group_name
+  location                    = var.region
+  storage_account_name        = var.storage_account_name
+  storage_account_access_key  = var.storage_account_access_key
+  service_plan_id             = azurerm_service_plan.app_service_plan.id
+  functions_extension_version = "~4"
   site_config {
     application_insights_key = var.application_insights_key
   }
   app_settings = {
     cosmosdb_account_connection_string = var.cosmosdb_account_connection_string
+    SCM_DO_BUILD_DURING_DEPLOYMENT     = true
   }
 }
