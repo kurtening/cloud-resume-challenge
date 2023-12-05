090b5c3cc1d106b0d5d521c680e4734edd5e8a98
kurtening <kurtranzelflores@gmail.com>
2023-12-05 10:57:53 +0800

    Add origin host header

--- iac/terraform/modules/cdn/main.tf
+++ iac/terraform/modules/cdn/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_cdn_endpoint" "cdn_endpoint" {
   profile_name        = azurerm_cdn_profile.cdn_profile.name
   location            = var.location
   resource_group_name = var.resource_group_name
-
+  origin_host_header = var.cdn_endpoint_origin_host_name
   origin {
     name      = var.cdn_endpoint_origin_name
     host_name = var.cdn_endpoint_origin_host_name
